8345198310a46a914dc8a16603eb64f50c954a3d
Vivansh Jaiswal <vivanshjaiswal14@gmail.com>
2026-04-09 23:24:30 +0530

    Time: 9 ms (9.15%) | Memory: 46.7 MB (11.83%) - LeetSync

--- 71-simplify-path/simplify-path.java
+++ 71-simplify-path/simplify-path.java
@@ -1,29 +1,24 @@
 class Solution {
     public String simplifyPath(String path) {
-        java.util.Stack<String> st = new java.util.Stack<>();
+        java.util.List<String> dirs = new java.util.ArrayList<>();
         String temp = "";
         for (int i = 0; i <= path.length(); i++) {
             if (i == path.length() || path.charAt(i) == '/') {
                 if (temp.equals("") || temp.equals(".")) {
                 } else if (temp.equals("..")) {
-                    if (!st.isEmpty()) st.pop();
+                    if (!dirs.isEmpty()) dirs.remove(dirs.size() - 1);
                 } else {
-                    st.push(temp);
+                    dirs.add(temp);
                 }
                 temp = "";
             } else {
                 temp += path.charAt(i);
             }
         }
-        java.util.List<String> res = new java.util.ArrayList<>();
-        while (!st.isEmpty()) {
-            res.add(st.pop());
+        StringBuilder res = new StringBuilder();
+        for (String dir : dirs) {
+            res.append("/").append(dir);
         }
-        java.util.Collections.reverse(res);
-        StringBuilder ans = new StringBuilder();
-        for (String dir : res) {
-            ans.append("/").append(dir);
-        }
-        return ans.length() == 0 ? "/" : ans.toString();
+        return res.length() == 0 ? "/" : res.toString();
     }
 }
