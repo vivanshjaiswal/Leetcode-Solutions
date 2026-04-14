524287e82d835d7f443b3b235484ca7ddeb9a010
Vivansh Jaiswal <vivanshjaiswal14@gmail.com>
2026-04-15 01:55:44 +0530

    Time: 16 ms (47.40%) | Memory: 42.6 MB (88.06%) - LeetSync

--- 457-circular-array-loop/circular-array-loop.java
+++ 457-circular-array-loop/circular-array-loop.java
@@ -3,27 +3,45 @@ class Solution {
         int n = nums.length;
 
         for (int i = 0; i < n; i++) {
+            if (nums[i] == 0) continue;
+
             boolean forward = nums[i] > 0;
-            Set<Integer> visited = new HashSet<>();
-            int current = i;
+            int slow = i, fast = i;
 
             while (true) {
-                if ((nums[current] > 0) != forward) break;
+                slow = getNext(nums, forward, slow);
+                fast = getNext(nums, forward, fast);
+                if (fast != -1) fast = getNext(nums, forward, fast);
 
-                if (visited.contains(current)) {
+                if (slow == -1 || fast == -1) break;
+
+                if (slow == fast) {
+                    if (slow == getNext(nums, forward, slow)) break;
                     return true;
                 }
+            }
 
-                visited.add(current);
-
-                int next = (current + nums[current]) % n;
+            int index = i;
+            while (nums[index] != 0) {
+                int next = (index + nums[index]) % n;
                 if (next < 0) next += n;
-
-                if (next == current) break;
-
-                current = next;
+                nums[index] = 0;
+                index = i;
             }
         }
         return false;
+    }
+
+    private int getNext(int[] nums, boolean forward, int index) {
+        boolean direction = nums[index] > 0;
+        if (direction != forward) return -1;
+
+        int n = nums.length;
+        int next = (index + nums[index]) % n;
+        if (next < 0) next += n;
+
+        if (next == index) return -1;
+
+        return next;
     }
 }
