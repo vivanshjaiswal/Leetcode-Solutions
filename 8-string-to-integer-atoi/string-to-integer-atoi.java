c327db49d917b0db167e19516cb060fc5bd57a3c
Vivansh Jaiswal <vivanshjaiswal14@gmail.com>
2026-04-24 16:20:42 +0530

    Time: 1 ms (100.00%) | Memory: 43.7 MB (61.39%) - LeetSync

--- 8-string-to-integer-atoi/string-to-integer-atoi.java
+++ 8-string-to-integer-atoi/string-to-integer-atoi.java
@@ -1,38 +1,27 @@
 class Solution {
-    static final int INT_MIN_VAL = -2147483648;
-    static final int INT_MAX_VAL = 2147483647;
-
-    // Recursive helper
-    static int helper(String s, int i, long num, int sign) {
-        // Base case: end or non-digit
-        if (i >= s.length() || !Character.isDigit(s.charAt(i)))
-            return (int)(sign * num);
-
-        // Update num
-        num = num * 10 + (s.charAt(i) - '0');
-
-        // Clamp overflow
-        if (sign * num <= INT_MIN_VAL) return INT_MIN_VAL;
-        if (sign * num >= INT_MAX_VAL) return INT_MAX_VAL;
-
-        // Recurse
-        return helper(s, i + 1, num, sign);
+    // static final int INT_MIN_VAL = -2147483648;
+    //     static final int INT_MAX_VAL = 2147483647;
+    private int helper(String s,long nums,int i,int sign){
+        
+        if(i>=s.length() || !Character.isDigit(s.charAt(i))){
+            return (int)(sign*nums);
+        }
+        nums=nums*10+s.charAt(i)-'0';
+        
+        if (sign == 1 && nums > Integer.MAX_VALUE) return Integer.MAX_VALUE;
+        if (sign == -1 && -nums < Integer.MIN_VALUE) return Integer.MIN_VALUE;
+        return helper(s,nums,i+1,sign);
     }
-
-    static int myAtoi(String s) {
-        int i = 0;
-
-        // Skip whitespaces
-        while (i < s.length() && s.charAt(i) == ' ') i++;
-
-        // Handle sign
-        int sign = 1;
-        if (i < s.length() && (s.charAt(i) == '+' || s.charAt(i) == '-')) {
-            sign = (s.charAt(i) == '-') ? -1 : 1;
+    public int myAtoi(String s) {
+        int i=0;
+        while(i<s.length() && s.charAt(i)==' '){
             i++;
         }
-
-        // Recursive helper
-        return helper(s, i, 0, sign);
+        int sign=1;
+        if(i<s.length() && (s.charAt(i)=='+'||s.charAt(i)=='-')){
+            sign=(s.charAt(i)=='-')?-1:1;
+            i++;
+        }
+        return helper(s,0,i,sign);
     }
 }
